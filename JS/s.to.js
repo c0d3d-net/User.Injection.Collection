431a474ca445fac9255e549ac5db8e2e80570215
c0d3d.net <35439168+c0d3d-net@users.noreply.github.com>
2018-12-18 07:29:42 +0100

    Update s.to.js

--- JS/s.to.js
+++ JS/s.to.js
@@ -1,9 +1,2 @@
-/* 
-Padding modification for this special "small" tag with that inline style defined in "var sml".
-
-Used for the S.TO Night TV Mode Modifications v2.0
-coded by c0d3d.net
-*/
-
 var sml = document.querySelector('small[style="color: #ddd; padding: 8px; display: block; line-height: 1.5; font-size: 14px; text-align: center;"]');
 sml.style.padding = "8px 8px 8px 16px";
