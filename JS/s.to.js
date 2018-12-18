0ad76e883b82802703c491baeeacbc2f30dbd74d
c0d3d.net <35439168+c0d3d-net@users.noreply.github.com>
2018-12-18 14:45:08 +0100

    Update s.to.js

--- JS/s.to.js
+++ JS/s.to.js
@@ -1,2 +1,4 @@
 var sml = document.querySelector('small[style="color: #ddd; padding: 8px; display: block; line-height: 1.5; font-size: 14px; text-align: center;"]');
 sml.style.padding = "8px 8px 8px 16px";
+
+$('link[rel=stylesheet][href^="/public/css/main.dark.min.css"]').remove();
