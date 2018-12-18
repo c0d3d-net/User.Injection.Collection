616415abb2c22f1197938830f6b9508d42ec428a
c0d3d.net <35439168+c0d3d-net@users.noreply.github.com>
2018-12-18 02:31:01 +0100

    Create rapidvideo.com.js

---
+++ JS/rapidvideo.com.js
@@ -0,0 +1,6 @@
+var ul1 = document.querySelector('div[style^="background-color:#242424; height:60px;"]');
+ul1.parentNode.removeChild(ul1);
+
+
+var ul0 = document.querySelector('div[style="background-color:#242424;border-top:1px solid #424242; border-bottom:1px solid #424242; line-height:70px;"]');
+ul0.parentNode.removeChild(ul0);
