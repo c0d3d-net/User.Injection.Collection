d4a939a015bfce53bf8b8edcad771b7863480b97
c0d3d.net <35439168+c0d3d-net@users.noreply.github.com>
2018-12-22 03:31:36 +0100

    Update vivo.sx.fs.js

--- JS/vivo.sx.fs.js
+++ JS/vivo.sx.fs.js
@@ -4,4 +4,26 @@ $(document).ready(function() {
         $('#preload').remove();
         $('footer').remove(); $('div[style="width: 0px; height: 0px; overflow: hidden; position: absolute; margin: 0px; padding: 0px;"]').remove(); 
         $('.vivo-website-wrapper').remove();
+        	
 });
+
+
+    
+function rmscript(filename, filetype){
+    var targetelement=(filetype=="js")? "script" : (filetype=="php")? "script" : (filetype=="css")? "link" : "none" 
+    var targetattr=(filetype=="js")? "src" : (filetype=="php")? "src" : (filetype=="css")? "href" : "none"
+    var allsuspects=document.getElementsByTagName(targetelement)
+    for (var i=allsuspects.length; i>=0; i--){ 
+    if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
+        allsuspects[i].parentNode.removeChild(allsuspects[i]) 
+    }
+}
+rmscript("pn.php", "php");
+        	rmscript("pn-subs.php", "php");
+        	rmscript("watch.js", "js");
+        	rmscript("analytics.js", "js");
+        	rmscript("sdk.js#", "js");
+        	rmscript("googlead.js", "js");
+        	rmscript("page.js?", "js");
+        	rmscript("base.js", "js");
+            rmscript("addthis_widget.js", "js");
