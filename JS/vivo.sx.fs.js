acd2b6f265db8dc27afd67967b40ec2e4bc2fff9
c0d3d.net <35439168+c0d3d-net@users.noreply.github.com>
2018-12-22 01:48:08 +0100

    Create vivo.sx.fs.js

---
+++ JS/vivo.sx.fs.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    	$('.fb_reset').replaceWith($('#player'));
+        $(".vivo-video-data-holder").remove();
+        $('#preload').remove();
+        $('footer').remove(); $('div[style="width: 0px; height: 0px; overflow: hidden; position: absolute; margin: 0px; padding: 0px;"]').remove(); 
+        $('.vivo-website-wrapper').remove();
+});
